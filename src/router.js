62edcba7a1943491e1055587c0228e18cf989dfe
isiagi <isiagigeofrey0@gmail.com>
2021-06-09 16:12:49 +0300

    add getById parcels

--- src/router.js
+++ src/router.js
@@ -1,9 +1,19 @@
 import express from 'express'
 import parcelModal from './models/parcel.model'
 import parcelController from './controllers/parcelController'
+import Response from './utils/response'
 
 const router = express.Router()
 
 router.get('/parcels', parcelController.getParcels)
+router.get('/parcel/:id', (req, res) =>{
+    parcelModal.findOne({_id: id},(err, result) => {
+        if(err){
+            Response(res, 400, err)
+        }
+        return Response(res, 200, result)
+    })
+
+})
 
 export default router
